255e26354ba07cc133df49e34ac37571852e4eb6
Pradeep Raj <60281056+MPradeepRaj02@users.noreply.github.com>
2021-02-02 11:49:01 +0530

    Update IGDPG.py

--- IGDPG.py
+++ IGDPG.py
@@ -3,13 +3,12 @@
 # BY PRADEEP RAJ (M.P.R)
 
 import instaloader
-from PIL import Image
+
 
 m = instaloader.Instaloader()
 
 dp = input('Type the Username : ')
 
 download = m.download_profile(dp,profile_pic_only=True)
-pil = Image.open(downloaded)
-pil.show()
+
 #m.download_tagged(dp)
